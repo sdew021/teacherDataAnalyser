e0c0872b09e53bf27d3a0258e81ab2d1653bb5c5
sdew021 <sdewangan021021@gmail.com>
2020-04-03 19:09:11 +0530

    added subjec list page and home page

--- app/src/main/java/com/example/dataanalyser/MainActivity.java
+++ app/src/main/java/com/example/dataanalyser/MainActivity.java
@@ -31,6 +31,14 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        bregister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,register.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
--- app/src/main/java/com/example/dataanalyser/homePage.java
+++ app/src/main/java/com/example/dataanalyser/homePage.java
@@ -2,7 +2,10 @@ package com.example.dataanalyser;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class homePage extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class homePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        Button next = findViewById(R.id.next);
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(homePage.this,subjectList.class);
+                startActivity(intent);
+            }
+        });
     }
 }
