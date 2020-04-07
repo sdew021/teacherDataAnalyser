407e0a64326d16c11cc59a33cc79819c6400cdd6
sdew021 <sdewangan021@gmail.com>
2020-04-07 19:13:25 +0530

    modified analysis page

--- app/src/main/java/com/example/dataanalyser/MainActivity.java
+++ app/src/main/java/com/example/dataanalyser/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -31,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ephone=(EditText)findViewById(R.id.phone);
         blogin=(Button)findViewById(R.id.login);
+
         blogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -39,10 +41,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-<<<<<<< HEAD
-
-        bregister.setOnClickListener(new View.OnClickListener() {
-=======
     }
 
     private void checkId(final String id, final Intent intent) {
@@ -55,7 +53,6 @@ public class MainActivity extends AppCompatActivity {
 
 
         myRef.addListenerForSingleValueEvent(new ValueEventListener() {
->>>>>>> 204ae1c981758cf2580be6a7c1eae8ea625f3e21
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 for (DataSnapshot childDataSnapshot : dataSnapshot.getChildren()) {
